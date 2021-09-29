c201fd4438ca69d2db4dcfddd6e4bd4fbe0c4695
randallalala <randall.lim.lim.lim@hotmail.com>
2021-09-29 07:47:04 +0000

    s3

---
+++ workshop3/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.12.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+  backend "s3" {
+    endpoint                    = "sgp1.digitaloceanspaces.com"
+    key                         = "workshop3/terraform.tfstate"
+    bucket                      = "aipc"
+    region                      = "sgp1"
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_get_ex2_plaforms       = true
+    skip_metadata_api_check     = true
+    access_key = "value"
+    secret_key = "value"
+  }
+}
+
+
+provider "digitalocean" {
+  token = var.DO_token
+}
+
+provider "local" {
+}
